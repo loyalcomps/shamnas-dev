28d087d7cc208da024aba7fc8652e9ec7cb915dd
SHAMNAS KOYANI <shamnaskoyani@gmail.com>
2021-05-26 16:16:59 +0530

    Basic module structure

--- first_module/__manifest__.py
+++ first_module/__manifest__.py
@@ -1,1 +1,12 @@
-{ 'name' : 'My First Module'}
+{
+'name': "BSS Management",
+'summary': "Manage BSS activities",
+'description': "Manage BSS Activities",
+'author': "Shamnas Koyani",
+'website': "http://www.shamnaskoyani.com",
+'category': 'Uncategorized',
+'version': '14.0.1',
+'depends': ['base'],
+'data': ['views/views.xml'],
+'demo': ['demo.xml'],
+}
